44fea17ebc86fdbf41a431133025e1dccb6118b4
AS Chollet <annesylvie.chollet@gmail.com>
2022-10-05 18:51:40 +0200

    feat(front) creation layout et pages authentification et salons
    SC-6

--- front/src/pages/auth.tsx
+++ front/src/pages/auth.tsx
@@ -1,11 +1,31 @@
+import { Button, Container, Input, Spacer, Card, Grid } from '@nextui-org/react';
 import React from 'react';
 
-const Auth= () => (
-	<section className="section">
-		<div className="container">
-			<h1>Authentification</h1>
-		</div>
-	</section>
+const Auth = () => (
+	<Grid.Container gap={2} justify="center">
+		<Grid xs={12} md={4}>
+			<Container>
+				<Card>
+					<Card.Body>
+						<form action="/send-data-here" method="post">
+							<Input placeholder='email' clearable width="100%" />
+							<Spacer y={1} />
+							<Input.Password placeholder="mot de passe" width="100%" />
+							<Spacer y={2} />
+							<Grid.Container gap={2} justify="center">
+								<Grid>
+									<Button flat auto>Créer un compte</Button>
+								</Grid>
+								<Grid>
+									<Button auto>S'authentifier</Button>
+								</Grid>
+							</Grid.Container>
+						</form>
+					</Card.Body>
+				</Card>
+			</Container>
+		</Grid>
+	</Grid.Container>
 );
 
 export default Auth;
--- front/src/pages/roomsList.tsx
+++ front/src/pages/roomsList.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import { Button, Input, Spacer, Card, Grid, Text, Row } from '@nextui-org/react';
 
-const RoomsList= () => (
-	<section className="section">
-		<div className="container">
-			<h1>Liste des salons disponibles</h1>
-		</div>
-	</section>
-);
+const roomListElement = (rooms: string[]) => (
+	<>
+		{rooms.map(roomName => {
+			return (
+				<Grid>
+					<Button shadow auto>{roomName}</Button>
+				</Grid>
+			)
+		})}
+	</>
+
+)
+
+const RoomsList = () => {
+
+	const salons = ["baleine", "pieuvre"];
+
+	return (
+		<Grid.Container gap={2} justify="center">
+			<Grid xs={12} md={6}>
+				<Card>
+					<Card.Header>
+						<Text
+							h2
+							size={30}
+							css={{
+								textGradient: "45deg, $cyan700 -20%, $blue300 50%",
+							}}
+							weight="bold"
+						>
+							Liste des salons disponibles
+						</Text>
+					</Card.Header>
+					<Card.Divider />
+					<Card.Body>
+						<Grid.Container gap={2} justify="center">
+							{roomListElement(salons)}
+						</Grid.Container>
+					</Card.Body>
+					<Card.Divider />
+					<Card.Footer>
+						<Row justify="flex-end">
+						<Input placeholder='nouveau' clearable width="100%" />
+							<Spacer x={1} />
+							<Button flat auto>Créer un nouveau salon</Button>
+						</Row>
+					</Card.Footer>
+				</Card>
+			</Grid>
+		</Grid.Container>
+	)
+};
 
 export default RoomsList;
