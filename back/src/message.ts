c46c9061db4f97cd1a9b30598affffae3218af25
AS Chollet <annesylvie.chollet@gmail.com>
2022-10-10 19:49:58 +0200

    feat(back) amelioration des routes pour correspondre aux besoins du front
    SC-7

--- back/src/message.ts
+++ back/src/message.ts
@@ -1,34 +1,18 @@
-import express, { Router , Request, Response} from 'express';
+import express, { Router, Request, Response } from 'express';
 import pgPool from './lib/pool';
 
-const router: Router = express.Router( {
+const router: Router = express.Router({
     mergeParams: true,
 });
 
-router.get("/", async (req: Request, res : Response) => {
+router.get("/", async (req: Request, res: Response) => {
 
     const client = await pgPool.connect();
-   
-    try {
-        const {roomId} = req.params;
-        const resQuery = await client.query('SELECT * from messages WHERE room = $1',[roomId]);
-        const { rows } = resQuery;
-        res.json(rows);
-        console.log(req.params);
-    } catch (e) {
-        res.status(500).send(e.toString());
-    } finally {
-        client.release();
-    }
-});
 
-router.get("/:messageId", async (req: Request, res : Response) => {
-
-    const client = await pgPool.connect();
-   
     try {
-        const {messageId} = req.params;
-        const resQuery = await client.query('SELECT * from messages WHERE id = $1',[messageId]);
+        const { roomId } = req.params;
+        const resQuery = await client.query(
+            'SELECT m.content, m.id, m.created_at, u.name, u.email, u.id AS user_id from messages AS m JOIN users AS u ON m.author=u.id WHERE m.room = $1', [roomId]);
         const { rows } = resQuery;
         res.json(rows);
     } catch (e) {
@@ -38,15 +22,32 @@ router.get("/:messageId", async (req: Request, res : Response) => {
     }
 });
 
-router.post("/", async (req: Request, res : Response) => {
-    
+router.get("/:messageId", async (req: Request, res: Response) => {
+
     const client = await pgPool.connect();
 
     try {
-        const {content, author} = req.body;
-        const {roomId} = req.params;
-        await client.query('INSERT INTO messages(room, content, author) VALUES($1, $2, $3) RETURNING *',[roomId, content, author]);
-        res.json({status: 'post message ok'});
+        const { messageId } = req.params;
+        const resQuery = await client.query('SELECT * from messages WHERE id = $1', [messageId]);
+        const { rows } = resQuery;
+        res.json(rows);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    } finally {
+        client.release();
+    }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+
+    const client = await pgPool.connect();
+
+    try {
+        const { content } = req.body;
+        const { roomId } = req.params;
+        const author = req.user_id;
+        await client.query('INSERT INTO messages(room, content, author) VALUES($1, $2, $3) RETURNING *', [roomId, content, author]);
+        res.json({ status: 'post message ok' });
     } catch (e) {
         res.status(500).send(e.toString());
     } finally {
--- back/src/user.ts
+++ back/src/user.ts
@@ -19,30 +19,14 @@ router.get("/", async (req: Request, res: Response) => {
     }
 });
 
-router.get("/:userId", enableAuth, async (req: Request, res: Response) => {
-
-    const client = await pgPool.connect();
-
-    try {
-        const { userId } = req.params;
-        const resQuery = await client.query('SELECT * from users WHERE id = $1', [userId]);
-        const { rows } = resQuery;
-        res.json(rows);
-    } catch (e) {
-        res.status(500).send(e.toString());
-    } finally {
-        client.release();
-    }
-});
-
 router.post("/", async (req: Request, res: Response) => {
 
     const client = await pgPool.connect();
 
     try {
-        const { name, email, password } = req.body;
+        const { email, password } = req.body;
         const hashedPassword = await hashPassword(password);
-        await client.query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *', [name, email, hashedPassword]);
+        await client.query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *', ["name", email, hashedPassword]);
         res.json({ status: 'post user ok' });
     } catch (e) {
         res.status(500).send(e.toString());
